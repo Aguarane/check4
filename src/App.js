93c319b8d785ece98e3d7281e54aed4b08359455
Aguarane <56790666+Aguarane@users.noreply.github.com>
2020-02-09 16:31:40 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import './aa.css'
+import './Photo'
+import './Title'
+import './wrapper'
+
+function App() {
+  return (
+    <Wrapper>
+
+	<Photo src="./button.png">
+
+	<Title style={{
+
+		color:"red"
+
+}}>My Name here</Title>
+
+	<Title small>My job here</Title>
+
+
+</Wrapper>
+
+  );
+}
+
+
+export default App
